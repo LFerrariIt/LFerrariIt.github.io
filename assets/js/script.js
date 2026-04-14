81b93deba687e220db2bf1990115eaff6ef21f3e
Luisa Ferrari <luisaferrari@unimore.it>
2026-04-14 11:38:10 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -146,21 +146,27 @@ const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
 // add event to all nav links
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
+navigationLinks.forEach(link => {
+  link.addEventListener("click", function () {
 
-    const selectedPage = this.innerText.toLowerCase();
+    const selectedPage = this.innerText.trim().toLowerCase();
 
-    for (let j = 0; j < pages.length; j++) {
-      if (selectedPage === pages[j].dataset.page) {
-        pages[j].classList.add("active");
-        navigationLinks[j].classList.add("active");
-        window.scrollTo(0, 0);
+    // toggle pages
+    pages.forEach(page => {
+      if (page.dataset.page === selectedPage) {
+        page.classList.add("active");
       } else {
-        pages[j].classList.remove("active");
-        navigationLinks[j].classList.remove("active");
+        page.classList.remove("active");
       }
-    }
+    });
 
+    // toggle nav buttons
+    navigationLinks.forEach(nav => {
+      nav.classList.remove("active");
+    });
+    this.classList.add("active");
+
+    window.scrollTo(0, 0);
   });
+});
 }
